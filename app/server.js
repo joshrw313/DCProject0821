aa70ab861ad2323d29dbb907c898fe8a2bfc8f4f
Josh White <joshrw313@gmail.com>
2021-08-09 17:22:19 -0400

    added google oauth2 passport strategy with express paths in server.js

--- app/server.js
+++ app/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
 var corsOptions = {
@@ -24,6 +29,68 @@ app.use(express.urlencoded({ extended: true }));
 
 const db = require("./models");
 const Role = db.role;
+const client = require('./config/google.Oauth.js');
+console.log(client);
+
+passport.use(new GoogleStrategy({
+	  clientID: "1011376484214-mv7mbq2n4cngg6p9lkvbfosoh49jc0pv.apps.googleusercontent.com",
+	  clientSecret: "PWrMsjFXVJIrW9JW8A4i7dUA",
+    callbackURL: "http://localhost:8080/api/auth/googleRedirect"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      //console.log(accessToken, refreshToken, profile)
+      console.log("GOOGLE BASED OAUTH VALIDATION GETTING CALLED")
+      return done(null, profile)
+  }
+))
+
+app.get('/api/auth/google',  passport.authenticate('google', { scope: ['profile','email'] }));
+
+app.get('api/auth/googleRedirect', passport.authenticate('google'),(req, res)=>{
+    console.log('redirected', req.user)
+    /*let user = {
+        displayName: req.user.displayName,
+        name: req.user.name.givenName,
+        email: req.user._json.email,
+        provider: req.user.provider }
+    console.log(user)
+   */ 
+  User.findOne({
+    where: {
+      username: req.user.displayName
+    }
+  })
+  .then(async (user) => {
+      if (!user) {
+        User.create({
+          username: req.user.displayName,
+          email: req.user._json.email,
+          password: null,
+          provider: req.user.provider 
+        })
+        res.status(200).send('User registered sucessfully').redirect('/api/auth/google');
+
+       }
+
+      const token = jwt.sign({ id: user.id }, config.secret, {
+        expiresIn: config.jwtExpiration
+      });
+
+      let refreshToken = await RefreshToken.createToken(user);
+      
+        res.status(200).send({
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          accessToken: token,
+          refreshToken: refreshToken,
+        });
+      })
+    .catch(err => {
+      res.status(500).send({ message: err.message });
+    });
+});
+
 
 db.sequelize.sync({force: true}).then(() => {
   console.log('Drop and Resync Db');
@@ -48,7 +115,7 @@ function initial() {
 }
 
 // simple route
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   res.render('main');
 });
 
