8cb49bea34ed645ff690dc0b57f82fffbb4ed215
Josh White <joshrw313@gmail.com>
2021-08-12 17:56:53 -0400

    moving google oauth2 code to be more in line with how the rest of the code is organized

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,7 +1,33 @@
 const { verifySignUp } = require("../middleware");
 const controller = require("../controllers/auth.controller");
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 module.exports = function(app) {
+const googleKey = require("../config/google.Oauth");
+
+passport.use(new GoogleStrategy({
+	  clientID: googleKey.clientId, 
+	  clientSecret: googleKey.clientSecret, 
+    callbackURL: "https://dcproject0821.xyz/api/auth/googleRedirect"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      //console.log(accessToken, refreshToken, profile)
+      console.log("GOOGLE BASED OAUTH VALIDATION GETTING CALLED")
+      return done(null, profile)
+  }
+));
+
+passport.serializeUser(function(user, done) {
+    console.log('serialize');
+    done(null, user)
+})
+passport.deserializeUser(function(obj, done) {
+    console.log(deserialize);
+    done(null, obj)
+})
+
+  app.use(passport.initialize());
   app.use(function(req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
@@ -21,4 +47,6 @@ module.exports = function(app) {
 
   app.post("/api/auth/signin", controller.signin);
   app.post("/api/auth/refreshtoken", controller.refreshToken);
+  app.get('/api/auth/google',  passport.authenticate('google', { scope: ['profile','email'] }));
+  app.get('/api/auth/googleRedirect', passport.authenticate('google'), controller.googleSignIn);
 };
