6c43b21cc14994e09ba9ce49e692a33915b4c2e9
Josh White <joshrw313@gmail.com>
2021-08-11 20:21:53 -0400

    added db table for comments

--- app/models/index.js
+++ app/models/index.js
@@ -29,6 +29,7 @@ db.role = require("./role.model.js")(sequelize, Sequelize);
 db.refreshToken = require("./refreshToken.model.js")(sequelize, Sequelize);
 db.board = require("./board.model.js")(sequelize, Sequelize);
 db.post = require("./post.model.js")(sequelize, Sequelize);
+db.comment = require("./comment.model.js")(sequelize, Sequelize);
 
 db.role.belongsToMany(db.user, {
   through: "user_roles",
@@ -49,15 +50,27 @@ db.user.hasOne(db.refreshToken, {
 db.user.hasMany(db.post, {
   foreignKey: 'userId', targetKey: 'id'
 });
+db.user.hasMany(db.comment, {
+  foreignKey: 'userId', targetKey: 'id'
+});
 db.board.hasMany(db.post, {
   foreignKey: 'boardId', targetKey: 'id'
 });
 db.post.belongsTo(db.user, {
   foreignKey: 'userId', targetKey: 'id'
 });
+db.comment.belongsTo(db.user, {
+  foreignKey: 'userId', targetKey: 'id'
+});
+db.comment.belongsTo(db.post, {
+  foreignKey: 'postId', targetKey: 'id'
+});
 db.post.belongsTo(db.board, {
   foreignKey: 'boardId', targetKey: 'id'
 })
+db.post.hasMany(db.comment, {
+  foreignKey: 'postId', targetKey: 'id'
+});
 
 db.ROLES = ["user", "admin", "moderator"];
 
